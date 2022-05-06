a0bb5f9a79bd90e9a2fd48d8c186e539a0398962
theo <theo.dillenseger@yahoo.fr>
2022-05-06 21:39:31 +0200

    build

---
+++ Client copie/build/precache-manifest.501f2241a42f9d32ba89f84ae1305b52.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3fcf438b9497e2d39fb42dad310d3ab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c19f6f3cf29f8d8bb38b",
+    "url": "/static/css/main.21c585e7.chunk.css"
+  },
+  {
+    "revision": "4c6c7928a551a9d7731b",
+    "url": "/static/js/2.8adde6fa.chunk.js"
+  },
+  {
+    "revision": "b67bc97fd0dd67004287af4028afc1b1",
+    "url": "/static/js/2.8adde6fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c19f6f3cf29f8d8bb38b",
+    "url": "/static/js/main.58c04c3f.chunk.js"
+  },
+  {
+    "revision": "85f04d63583017a2a039",
+    "url": "/static/js/runtime-main.cfd8bb26.js"
+  },
+  {
+    "revision": "7ce56587a9508088fbcf86e2e638d65d",
+    "url": "/static/media/Paradiso.7ce56587.ttf"
+  }
+]);
